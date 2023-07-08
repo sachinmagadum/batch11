0753ed32cbe07f179929f9115f410e3507b304f0
sachinmagadum <sachinmagadum5009@gmail.com>
2023-07-08 18:06:39 +0530

    Added code for Sub

--- arithmatic.py
+++ arithmatic.py
@@ -2,7 +2,13 @@ def addition(a,b):
     return a + b
 
 
+# code added by Digvijay Sir
+def subtraction(a,b):
+    return a - b
+
+
 a = 10
 b = 20
 
 print(f"Addition of {a} and {b} is {addition(a,b)}")
+print(f"Subtraction of {a} and {b} is {subtraction(a,b)}")
