58ba9c9d2eacead485caab13b39e3d94c3c25580
sachinmagadum <sachinmagadum5009@gmail.com>
2023-07-08 18:38:01 +0530

    modified code as per review comments

--- arithmatic.py
+++ arithmatic.py
@@ -3,7 +3,7 @@ def addition(a,b):
 
 
 # code added by Digvijay Sir
-def subtraction(a,b):
+def subtraction_of_two_nos(a,b):
     return a - b
 
 
@@ -11,4 +11,4 @@ a = 10
 b = 20
 
 print(f"Addition of {a} and {b} is {addition(a,b)}")
-print(f"Subtraction of {a} and {b} is {subtraction(a,b)}")
+print(f"Subtraction of {a} and {b} is {subtraction_of_two_nos(a,b)}")
